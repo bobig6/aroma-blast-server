ac166cf702940c1289d91cacaf37b38c94d1fa22
Boyan Ivanov <b.i.ivanov1@gmail.com>
2024-01-10 15:58:25 +0200

    Added points for chance and counting

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query, HttpException, HttpStatus } from '@nestjs/common';
+import { Controller, Get, Post, Query, HttpException, HttpStatus } from '@nestjs/common';
 import { AppService } from './app.service';
 import * as bcrypt from 'bcrypt';
 import * as fs from 'fs';
@@ -7,6 +7,7 @@ import * as path from 'path';
 @Controller()
 export class AppController {
   private readonly PROMO_CODES_FILE = path.join(__dirname, '../data/data.txt');
+  private readonly PARAMS_FILE = path.join(__dirname, '../data/params.json');
   private readonly accessTokenHash: string;
 
   constructor(private readonly appService: AppService) {
@@ -29,6 +30,108 @@ export class AppController {
     }
 
     return this.readAndDeletePromoCode();
+  }
+
+  @Get('promo-chance')
+  getPromoChance(@Query('accessToken') accessToken: string): number {
+    if (!accessToken) {
+      throw new HttpException('Access token is required', HttpStatus.BAD_REQUEST);
+    }
+
+    if (!bcrypt.compareSync(accessToken, this.accessTokenHash)) {
+      throw new HttpException('Invalid access token', HttpStatus.UNAUTHORIZED);
+    }
+
+    try {
+      const paramsData = fs.readFileSync(this.PARAMS_FILE, { encoding: 'utf8' });
+      const params = JSON.parse(paramsData);
+
+      if (typeof params.codeChance !== 'number') {
+        throw new Error('Invalid format for codeChance');
+      }
+
+      return params.codeChance;
+    } catch (err) {
+      throw new HttpException('Unable to read promo chance', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  @Post('increment-50-button')
+  increment50Button(@Query('accessToken') accessToken: string): HttpStatus {
+    if (!accessToken) {
+      throw new HttpException('Access token is required', HttpStatus.BAD_REQUEST);
+    }
+
+    if (!bcrypt.compareSync(accessToken, this.accessTokenHash)) {
+      throw new HttpException('Invalid access token', HttpStatus.UNAUTHORIZED);
+    }
+    return this.incrementButton('level50Presses');
+  }
+
+  @Post('increment-80-button')
+  increment80Button(@Query('accessToken') accessToken: string): HttpStatus {
+    if (!accessToken) {
+      throw new HttpException('Access token is required', HttpStatus.BAD_REQUEST);
+    }
+
+    if (!bcrypt.compareSync(accessToken, this.accessTokenHash)) {
+      throw new HttpException('Invalid access token', HttpStatus.UNAUTHORIZED);
+    }
+    return this.incrementButton('level80Presses');
+  }
+
+  @Get('get-50-button')
+  get50Button(@Query('accessToken') accessToken: string): number {
+    if (!accessToken) {
+      throw new HttpException('Access token is required', HttpStatus.BAD_REQUEST);
+    }
+
+    if (!bcrypt.compareSync(accessToken, this.accessTokenHash)) {
+      throw new HttpException('Invalid access token', HttpStatus.UNAUTHORIZED);
+    }
+    return this.getButtonCount('level50Presses');
+  }
+
+  @Get('get-80-button')
+  get80Button(@Query('accessToken') accessToken: string): number {
+    if (!accessToken) {
+      throw new HttpException('Access token is required', HttpStatus.BAD_REQUEST);
+    }
+
+    if (!bcrypt.compareSync(accessToken, this.accessTokenHash)) {
+      throw new HttpException('Invalid access token', HttpStatus.UNAUTHORIZED);
+    }
+    return this.getButtonCount('level80Presses');
+  }
+
+  private getButtonCount(buttonType: 'level50Presses' | 'level80Presses'): number {
+    try {
+      const paramsData = fs.readFileSync(this.PARAMS_FILE, { encoding: 'utf8' });
+      const params = JSON.parse(paramsData);
+
+      return params[buttonType] || 0;
+    } catch (err) {
+      throw new HttpException('Unable to read button count', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  private incrementButton(buttonType: 'level50Presses' | 'level80Presses'): HttpStatus {
+    try {
+      const paramsData = fs.readFileSync(this.PARAMS_FILE, { encoding: 'utf8' });
+      const params = JSON.parse(paramsData);
+
+      if (!params[buttonType]) {
+        params[buttonType] = 0;
+      }
+
+      params[buttonType]++;
+
+      fs.writeFileSync(this.PARAMS_FILE, JSON.stringify(params, null, 2));
+
+      return HttpStatus.OK;
+    } catch (err) {
+      throw new HttpException('Unable to increment button count', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   private async readAndDeletePromoCode(): Promise<string> {
